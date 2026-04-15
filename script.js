0088de18507dfb96c28db1439ee8ad04c3e413a5
gistyaputra-cmyk <gistyaputra@gmail.com>
2026-04-15 14:54:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function showSection(id){
+    document.getElementById("about").style.display = "none";
+    document.getElementById("market").style.display = "none";
+
+    document.getElementById(id).style.display = "block";
+}
+
+function logout(){
+    window.location.href = "index.html";
+}
+
+function goJual(){
+    window.location.href = "jual.html";
+}
+
+function showProduct(name){
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("productName").innerText = name;
+}
+
+function closePopup(){
+    document.getElementById("popup").style.display = "none";
+}
